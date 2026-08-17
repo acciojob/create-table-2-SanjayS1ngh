a1232040225d5b386f05bc3072a1fc5dc634aeea
Sanjay Singh <114378504+SanjayS1ngh@users.noreply.github.com>
2026-08-17 22:26:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function createTable() {
     //Write your code here
+	let table=document.querySelector("#myTable")
 	let body=document.querySelector("body")
-	let rn=prompt("Input number of rows")
-	let cn=prompt("Input number of columns")
-	let table=document.createElement("table")
-	
+	let rn=prompt(Number("Input number of rows"))
+	if(isNaN(rn) || rn<= 0){
+		alert("can not create the table, not a valid input ");
+		return;
+	}
+	let cn=prompt(Number("Input number of columns"))
+	if(isNaN(cn) || cn<= 0){
+		alert("can not create the table, not a valid input ");
+		return;
+	}
 	for(let i=0;i<rn;i++){
 		let row=document.createElement("tr")
 		for(let j=0;j<cn;j++){
